b0e64c7261662a4708da621fb500db62fa1aeab4
Cristian Tafolla Rosales <cristian.tafolla.rosales@gmail.com>
2019-05-23 11:03:04 -0500

    ADD: agregando :v

---
+++ creditos.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Creditos</title>
+	</head>
+	<body>
+		<h1>Créditos</h1>
+	        <h2>El que echo a perder la página</h2>
+		<p>Cristian Tafolla Rosales</p>	
+		<br>
+		<p>Aqui casual, reprobando.</p>
+		<br>
+		<br>
+		<a href="index.php">Inicio</a>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>El principito</title>
+	</head>
+	<body>
+		<h1>Libro "El principito"</h1>
+		<h2>Autor: Antoine de Saint-Exupéry</h2>
+		<p>libro</p>
+		<br>
+		<a href="creditos.php">Créditos</a>
+	</body>
+</html>
